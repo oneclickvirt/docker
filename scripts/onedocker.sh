11121ef3cc8f64cf20cdc4c5d98d55f19e6a759a
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-04-29 14:10:01 +0800

    Update onedocker.sh

--- scripts/onedocker.sh
+++ scripts/onedocker.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 #from https://github.com/spiritLHLS/docker
+
+name="$1"
+docker run -d --memory=512m -e USERNAME=test -e PASSWORD=123456 --name test --tmpfs /tmp:rw,size=5096000  -p 2022:22 debian /bin/bash -c "tail -f /dev/null && apt update && apt install wget sudo curl -y"
+docker cp ssh.sh ${name}:/ssh.sh
+docker exec -it ${name} bash -c "bash /ssh.sh"
