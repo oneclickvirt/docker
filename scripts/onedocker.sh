c40bd908bee42f7200f07f130f2d13a44def3710
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-04-29 21:34:20 +0800

    Update onedocker.sh

--- scripts/onedocker.sh
+++ scripts/onedocker.sh
@@ -17,12 +17,13 @@ if [ ! -f ssh.sh ]; then
     chmod 777 ssh.sh
     dos2unix ssh.sh
 fi
+# -v /var/run/docker.sock:/var/run/docker.sock
 if lsmod | grep -q xfs; then
   disk="$7"
-  docker run -d --cpus=${cpu} --memory=${memory}m --storage-opt size=${disk}G --name ${name} -p ${sshport}:22 -p ${startport}-${endport}:${startport}-${endport} --cap-add=NET_ADMIN --cap-add=SYS_ADMIN --cap-add=MKNOD -v /var/run/docker.sock:/var/run/docker.sock debian /bin/bash -c "tail -f /dev/null"
+  docker run -d --cpus=${cpu} --memory=${memory}m --storage-opt size=${disk}G --name ${name} -p ${sshport}:22 -p ${startport}-${endport}:${startport}-${endport} --cap-add=NET_ADMIN --cap-add=SYS_ADMIN --cap-add=MKNOD debian /bin/bash -c "tail -f /dev/null"
   echo "$name $sshport $passwd $cpu $memory $startport $endport" >> "$name"
 else
-  docker run -d --cpus=${cpu} --memory=${memory}m --name ${name} -p ${sshport}:22 -p ${startport}-${endport}:${startport}-${endport} --cap-add=NET_ADMIN --cap-add=SYS_ADMIN --cap-add=MKNOD -v /var/run/docker.sock:/var/run/docker.sock debian /bin/bash -c "tail -f /dev/null"
+  docker run -d --cpus=${cpu} --memory=${memory}m --name ${name} -p ${sshport}:22 -p ${startport}-${endport}:${startport}-${endport} --cap-add=NET_ADMIN --cap-add=SYS_ADMIN --cap-add=MKNOD debian /bin/bash -c "tail -f /dev/null"
   echo "$name $sshport $passwd $cpu $memory $startport $endport $disk" >> "$name"
 fi
 docker cp ssh.sh ${name}:/ssh.sh
