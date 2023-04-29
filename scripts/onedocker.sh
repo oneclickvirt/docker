79b42c2963f38ebc332f6bcdefe51a07b39e43e1
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-04-29 17:55:19 +0800

    Update onedocker.sh

--- scripts/onedocker.sh
+++ scripts/onedocker.sh
@@ -9,8 +9,11 @@ endport="$5"
 if lsmod | grep -q xfs; then
   disk="$6"
   docker run -d --memory=512m --storage-opt size=${disk}G --name ${name} -p ${sshport}:22 -p ${startport}-${endport}:${startport}-${endport} debian /bin/bash -c "tail -f /dev/null"
+  echo "$name $sshport $passwd $startport $endport" >> "$name"
 else
   docker run -d --memory=512m --name ${name} -p ${sshport}:22 -p ${startport}-${endport}:${startport}-${endport} debian /bin/bash -c "tail -f /dev/null"
+  echo "$name $sshport $passwd $startport $endport $disk" >> "$name"
 fi
 docker cp ssh.sh ${name}:/ssh.sh
 docker exec -it ${name} bash -c "bash /ssh.sh ${passwd}"
+cat "$name"
