779e5c5a6bb00fb85ded565a9eb736b4a153b4f2
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-04-29 18:13:16 +0800

    Update onedocker.sh

--- scripts/onedocker.sh
+++ scripts/onedocker.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 #from https://github.com/spiritLHLS/docker
 
-# ./onedocker.sh name memory passwd sshport startport endport <disk>
+# ./onedocker.sh name cpu memory sshport startport endport <disk>
 
 name="$1"
-memory="$2"
-passwd="$3"
+cpu="$2"
+memory="$3"
+ori=$(date | md5sum)
+passwd=${ori: 2: 9}
 sshport="$4"
 startport="$5"
 endport="$6"
 if lsmod | grep -q xfs; then
   disk="$7"
-  docker run -d --memory=512m --storage-opt size=${disk}G --name ${name} -p ${sshport}:22 -p ${startport}-${endport}:${startport}-${endport} debian /bin/bash -c "tail -f /dev/null"
-  echo "$name $memory $sshport $passwd $startport $endport" >> "$name"
+  docker run -d --cpus=${cpu} --memory=${memory}m --storage-opt size=${disk}G --name ${name} -p ${sshport}:22 -p ${startport}-${endport}:${startport}-${endport} debian /bin/bash -c "tail -f /dev/null"
+  echo "$name $sshport $passwd $cpu $memory $startport $endport" >> "$name"
 else
-  docker run -d --memory=512m --name ${name} -p ${sshport}:22 -p ${startport}-${endport}:${startport}-${endport} debian /bin/bash -c "tail -f /dev/null"
-  echo "$name $memory $sshport $passwd $startport $endport $disk" >> "$name"
+  docker run -d --cpus=${cpu} --memory=${memory}m --name ${name} -p ${sshport}:22 -p ${startport}-${endport}:${startport}-${endport} debian /bin/bash -c "tail -f /dev/null"
+  echo "$name $sshport $passwd $cpu $memory $startport $endport $disk" >> "$name"
 fi
 docker cp ssh.sh ${name}:/ssh.sh
 docker exec -it ${name} bash -c "bash /ssh.sh ${passwd}"
