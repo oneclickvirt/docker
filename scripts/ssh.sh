6a7054cd3c40a3c6ec3579d2c43218d9fd6bc0d0
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-06-08 12:23:05 +0800

    Update ssh.sh

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# by https://github.com/spiritLHLS/pve
+# by https://github.com/spiritLHLS/docker
 
 REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|alma|rocky" "'amazon linux'" "fedora" "arch")
 RELEASE=("Debian" "Ubuntu" "CentOS" "CentOS" "Fedora" "Arch")
@@ -9,6 +9,7 @@ PACKAGE_REMOVE=("apt-get -y remove" "apt-get -y remove" "yum -y remove" "yum -y 
 PACKAGE_UNINSTALL=("apt-get -y autoremove" "apt-get -y autoremove" "yum -y autoremove" "yum -y autoremove" "yum -y autoremove" "")
 CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)" "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)" "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)" "$(grep . /etc/redhat-release 2>/dev/null)" "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')" "$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)") 
 SYS="${CMD[0]}"
+temp_file_apt_fix="./apt_fix.txt"
 [[ -n $SYS ]] || exit 1
 for ((int = 0; int < ${#REGEX[@]}; int++)); do
     if [[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[int]} ]]; then
@@ -16,24 +17,52 @@ for ((int = 0; int < ${#REGEX[@]}; int++)); do
         [[ -n $SYSTEM ]] && break
     fi
 done
-${PACKAGE_UPDATE[int]}
-if ! command -v wget > /dev/null 2>&1; then
-    ${PACKAGE_INSTALL[int]} wget
+
+apt-get update -y
+if [ $? -ne 0 ]; then
+   dpkg --configure -a
+   apt-get update -y
 fi
-if ! command -v curl > /dev/null 2>&1; then
-    ${PACKAGE_INSTALL[int]} curl
+if [ $? -ne 0 ]; then
+   apt-get install gnupg -y
 fi
-if ! command -v sudo > /dev/null 2>&1; then
-    ${PACKAGE_INSTALL[int]} sudo
+apt_update_output=$(apt-get update 2>&1)
+echo "$apt_update_output" > "$temp_file_apt_fix"
+if grep -q 'NO_PUBKEY' "$temp_file_apt_fix"; then
+    public_keys=$(grep -oE 'NO_PUBKEY [0-9A-F]+' "$temp_file_apt_fix" | awk '{ print $2 }')
+    joined_keys=$(echo "$public_keys" | paste -sd " ")
+    echo "No Public Keys: ${joined_keys}"
+    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ${joined_keys}
+    apt-get update
+    if [ $? -eq 0 ]; then
+        echo "Fixed"
+    fi
 fi
+rm "$temp_file_apt_fix"
+
+install_required_modules() {
+    modules=("dos2unix" "wget" "sudo" "sshpass" "openssh-server")
+    for module in "${modules[@]}"
+    do
+        if dpkg -s $module > /dev/null 2>&1 ; then
+            echo "$module 已经安装！"
+        else
+            apt-get install -y $module
+	    if [ $? -ne 0 ]; then
+	        apt-get install -y $module --fix-missing
+	    fi
+            echo "$module 已尝试过安装！"
+        fi
+    done
+}
+install_required_modules
+
 sshport=22
 service iptables stop 2> /dev/null ; chkconfig iptables off 2> /dev/null ;
 sed -i.bak '/^SELINUX=/cSELINUX=disabled' /etc/sysconfig/selinux;
 sed -i.bak '/^SELINUX=/cSELINUX=disabled' /etc/selinux/config;
 setenforce 0;
 echo root:"$1" |sudo chpasswd root;
-apt-get install sshpass -y;
-apt-get install openssh-server -y;
 sed -i "s/^#\?Port.*/Port $sshport/g" /etc/ssh/sshd_config;
 sed -i "s/^#\?PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config;
 sed -i "s/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g" /etc/ssh/sshd_config;
