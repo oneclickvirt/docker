cb03df9d9eadbcc2aa97c021edbc2019be4655af
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-08-11 16:43:26 +0800

    Create docker_android.sh

---
+++ scripts/docker_android.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#from https://github.com/spiritLHLS/docker
+
+_red() { echo -e "\033[31m\033[01m$@\033[0m"; }
+_green() { echo -e "\033[32m\033[01m$@\033[0m"; }
+_yellow() { echo -e "\033[33m\033[01m$@\033[0m"; }
+_blue() { echo -e "\033[36m\033[01m$@\033[0m"; }
+reading(){ read -rp "$(_green "$1")" "$2"; }
+export DEBIAN_FRONTEND=noninteractive
+utf8_locale=$(locale -a 2>/dev/null | grep -i -m 1 -E "UTF-8|utf8")
+if [[ -z "$utf8_locale" ]]; then
+  echo "No UTF-8 locale found"
+else
+  export LC_ALL="$utf8_locale"
+  export LANG="$utf8_locale"
+  export LANGUAGE="$utf8_locale"
+  echo "Locale set to $utf8_locale"
+fi
+if [ "$(id -u)" != "0" ]; then
+   _red "This script must be run as root" 1>&2
+   exit 1
+fi
+REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|alma|rocky" "'amazon linux'" "fedora" "arch")
+RELEASE=("Debian" "Ubuntu" "CentOS" "CentOS" "Fedora" "Arch")
+PACKAGE_UPDATE=("! apt-get update && apt-get --fix-broken install -y && apt-get update" "apt-get update" "yum -y update" "yum -y update" "yum -y update" "pacman -Sy")
+PACKAGE_INSTALL=("apt-get -y install" "apt-get -y install" "yum -y install" "yum -y install" "yum -y install" "pacman -Sy --noconfirm --needed")
+PACKAGE_REMOVE=("apt-get -y remove" "apt-get -y remove" "yum -y remove" "yum -y remove" "yum -y remove" "pacman -Rsc --noconfirm")
+PACKAGE_UNINSTALL=("apt-get -y autoremove" "apt-get -y autoremove" "yum -y autoremove" "yum -y autoremove" "yum -y autoremove" "")
+CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)" "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)" "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)" "$(grep . /etc/redhat-release 2>/dev/null)" "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')" "$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)") 
+SYS="${CMD[0]}"
+[[ -n $SYS ]] || exit 1
+for ((int = 0; int < ${#REGEX[@]}; int++)); do
+    if [[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[int]} ]]; then
+        SYSTEM="${RELEASE[int]}"
+        [[ -n $SYSTEM ]] && break
+    fi
+done
+
+current_kernel_version=$(uname -r)
+target_kernel_version="5.0"
+if [[ "$(echo -e "$current_kernel_version\n$target_kernel_version" | sort -V | head -n 1)" == "$target_kernel_version" ]]; then
+    echo "当前内核版本 $current_kernel_version 大于或等于 $target_kernel_version，无需升级。"
+else
+    echo "当前内核版本 $current_kernel_version 小于 $target_kernel_version，请自行升级系统"
+fi
+if ! dpkg -S linux-modules-extra-${current_kernel_version} > /dev/null 2>&1; then
+    ${PACKAGE_INSTALL[int]} linux-modules-extra-${current_kernel_version}
+fi
+modprobe binder_linux devices="binder,hwbinder,vndbinder"
+modprobe ashmem_linux
+
+# https://hub.docker.com/r/redroid/redroid/tags
+# docker run -itd \
+#     --memory-swappiness=0 \
+#     --privileged --pull always \
+#     -v /root/test/data:/data \
+#     -p 55555:5555 \
+#     redroid/redroid:13.0.0-latest \
+#     androidboot.hardware=mt6891 ro.secure=0 ro.boot.hwc=GLOBAL    ro.ril.oem.imei=861503068361145 ro.ril.oem.imei1=861503068361145 ro.ril.oem.imei2=861503068361148 ro.ril.miui.imei0=861503068361148 ro.product.manufacturer=Xiaomi ro.build.product=chopin \
+#     redroid.width=720 redroid.height=1280 \
+#     redroid.gpu.mode=guest \
+#     --rm
