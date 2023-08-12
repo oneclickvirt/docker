8468c41742515485805cc02b75cbd0d2172286df
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-08-13 07:45:29 +0800

    Update startup.sh

--- extra_scripts/startup.sh
+++ extra_scripts/startup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
-
 set -eou pipefail
 chown root:kvm /dev/kvm
-systemctl enable libvirtd virtlogd --now
+service libvirtd start
+service virtlogd start
 VAGRANT_DEFAULT_PROVIDER=libvirt vagrant up
 iptables-save > $HOME/firewall.txt
 iptables -X
