3193d4eb640bc6a62001e7ab0b446968869be637
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-08-14 09:42:32 +0800

    Update startup.sh

--- extra_scripts/startup.sh
+++ extra_scripts/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # from 
 # https://github.com/spiritLHLS/docker
-# 2023.08.13
+# 2023.08.14
 
 
 set -eou pipefail
@@ -16,6 +16,8 @@ EOF
 systemctl enable libvirtd virtlogd --now
 VAGRANT_DEFAULT_PROVIDER=libvirt vagrant up 
 # --debug
+rdp_info=$(vagrant rdp 2>&1)
+ip_address=$(echo "$rdp_info" | grep -oP 'Address: (\d+\.\d+\.\d+\.\d+)' | grep -oP '(\d+\.\d+\.\d+\.\d+)')
 iptables-save > $HOME/firewall.txt
 iptables -X
 iptables -t nat -F
@@ -28,8 +30,8 @@ iptables -P OUTPUT ACCEPT
 iptables -A FORWARD -i eth0 -o virbr1 -p tcp --syn --dport 3389 -m conntrack --ctstate NEW -j ACCEPT
 iptables -A FORWARD -i eth0 -o virbr1 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A FORWARD -i virbr1 -o eth0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 3389 -j DNAT --to-destination 192.168.121.68
-iptables -t nat -A POSTROUTING -o virbr1 -p tcp --dport 3389 -d 192.168.121.68 -j SNAT --to-source 192.168.121.1
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 3389 -j DNAT --to-destination $ip_address
+iptables -t nat -A POSTROUTING -o virbr1 -p tcp --dport 3389 -d $ip_address -j SNAT --to-source 192.168.121.1
 iptables -D FORWARD -o virbr1 -j REJECT --reject-with icmp-port-unreachable
 iptables -D FORWARD -i virbr1 -j REJECT --reject-with icmp-port-unreachable
 iptables -D FORWARD -o virbr0 -j REJECT --reject-with icmp-port-unreachable
