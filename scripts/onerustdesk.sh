08315dc34a7401633f83c8d18b336ff1bc46871c
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-08-19 17:16:51 +0800

    Create onerustdesk.sh

---
+++ scripts/onerustdesk.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+# from 
+# https://github.com/spiritLHLS/docker
+# 2023.08.19
+
+cd /root >/dev/null 2>&1
+_red() { echo -e "\033[31m\033[01m$@\033[0m"; }
+_green() { echo -e "\033[32m\033[01m$@\033[0m"; }
+_yellow() { echo -e "\033[33m\033[01m$@\033[0m"; }
+_blue() { echo -e "\033[36m\033[01m$@\033[0m"; }
+reading(){ read -rp "$(_green "$1")" "$2"; }
+export DEBIAN_FRONTEND=noninteractive
+utf8_locale=$(locale -a 2>/dev/null | grep -i -m 1 -E "UTF-8|utf8")
+if [[ -z "$utf8_locale" ]]; then
+  echo "No UTF-8 locale found"
+else
+  export LC_ALL="$utf8_locale"
+  export LANG="$utf8_locale"
+  export LANGUAGE="$utf8_locale"
+  echo "Locale set to $utf8_locale"
+fi
+if [ "$(id -u)" != "0" ]; then
+   _red "This script must be run as root" 1>&2
+   exit 1
+fi
+REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|alma|rocky" "'amazon linux'" "fedora" "arch")
+RELEASE=("Debian" "Ubuntu" "CentOS" "CentOS" "Fedora" "Arch")
+PACKAGE_UPDATE=("! apt-get update && apt-get --fix-broken install -y && apt-get update" "apt-get update" "yum -y update" "yum -y update" "yum -y update" "pacman -Sy")
+PACKAGE_INSTALL=("apt-get -y install" "apt-get -y install" "yum -y install" "yum -y install" "yum -y install" "pacman -Sy --noconfirm --needed")
+PACKAGE_REMOVE=("apt-get -y remove" "apt-get -y remove" "yum -y remove" "yum -y remove" "yum -y remove" "pacman -Rsc --noconfirm")
+PACKAGE_UNINSTALL=("apt-get -y autoremove" "apt-get -y autoremove" "yum -y autoremove" "yum -y autoremove" "yum -y autoremove" "")
+CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)" "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)" "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)" "$(grep . /etc/redhat-release 2>/dev/null)" "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')" "$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)") 
+SYS="${CMD[0]}"
+[[ -n $SYS ]] || exit 1
+for ((int = 0; int < ${#REGEX[@]}; int++)); do
+    if [[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[int]} ]]; then
+        SYSTEM="${RELEASE[int]}"
+        [[ -n $SYSTEM ]] && break
+    fi
+done
+
+check_ipv4(){
+    API_NET=("ip.sb" "ipget.net" "ip.ping0.cc" "https://ip4.seeip.org" "https://api.my-ip.io/ip" "https://ipv4.icanhazip.com" "api.ipify.org")
+    for p in "${API_NET[@]}"; do
+        response=$(curl -s4m8 "$p")
+        sleep 1
+        if [ $? -eq 0 ] && ! echo "$response" | grep -q "error"; then
+            IP_API="$p"
+            break
+        fi
+    done
+    ! curl -s4m8 $IP_API | grep -q '\.' && red " ERROR：The host must have IPv4. " && exit 1
+    IPV4=$(curl -s4m8 "$IP_API")
+}
+
+check_ipv4
+if ! command -v docker > /dev/null 2>&1; then
+    _yellow "There is no Docker environment on this machine, please execute the main installation first."
+    _yellow "没有Docker环境，请先执行主体安装"
+    exit 1
+fi
+if ! command -v iptables > /dev/null 2>&1; then
+    ${PACKAGE_INSTALL[int]} iptables
+fi
+_green "Is a web client required? [Y]/N"
+reading "是否需要web客户端？[Y]/N" is_web_client
+docker image pull rustdesk/rustdesk-server
+is_web_client_lower=$(echo "$is_external_ip" | tr '[:upper:]' '[:lower:]')
+if [ "$is_web_client_lower" = "n" ]; then
+    web_hbbs_text="-p 21118:21118"
+    web_hbbr_text="-p 21119:21119"
+else
+    web_hbbs_text=""
+    web_hbbr_text=""
+fi
+docker run --name hbbs -p 21115:21115 -p 21116:21116 -p 21116:21116/udp ${web_hbbs_text} -v `pwd`:/root -td --net=host rustdesk/rustdesk-server hbbs -r ${IPV4}
+docker run --name hbbr -p 21117:21117 ${web_hbbr_text} -v `pwd`:/root -td --net=host rustdesk/rustdesk-server hbbr
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp --match multiport --sports 21115:21119 --dport 21115:21119 -j ACCEPT
+iptables -A INPUT -p tcp --dport 8000 -j ACCEPT
+iptables -A INPUT -p udp --dport 21116 -j ACCEPT
+iptables-save > /etc/iptables/rules.v4
+${PACKAGE_INSTALL[int]} iptables-persistent
+service netfilter-persistent restart
