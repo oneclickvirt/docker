1f2d9fdbfb306dc76f7c76578099203e9ac67e92
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-08-24 03:01:20 +0000

    update

--- scripts/dockerinstall.sh
+++ scripts/dockerinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # from
 # https://github.com/spiritLHLS/docker
-# 2023.08.15
+# 2023.08.24
 
 _red() { echo -e "\033[31m\033[01m$@\033[0m"; }
 _green() { echo -e "\033[32m\033[01m$@\033[0m"; }
@@ -68,18 +68,134 @@ check_update() {
     fi
 }
 
+check_interface() {
+    if [ -z "$interface_2" ]; then
+        interface=${interface_1}
+        return
+    elif [ -n "$interface_1" ] && [ -n "$interface_2" ]; then
+        if ! grep -q "$interface_1" "/etc/network/interfaces" && ! grep -q "$interface_2" "/etc/network/interfaces" && [ -f "/etc/network/interfaces.d/50-cloud-init" ]; then
+            if grep -q "$interface_1" "/etc/network/interfaces.d/50-cloud-init" || grep -q "$interface_2" "/etc/network/interfaces.d/50-cloud-init"; then
+                if ! grep -q "$interface_1" "/etc/network/interfaces.d/50-cloud-init" && grep -q "$interface_2" "/etc/network/interfaces.d/50-cloud-init"; then
+                    interface=${interface_2}
+                    return
+                elif ! grep -q "$interface_2" "/etc/network/interfaces.d/50-cloud-init" && grep -q "$interface_1" "/etc/network/interfaces.d/50-cloud-init"; then
+                    interface=${interface_1}
+                    return
+                fi
+            fi
+        fi
+        if grep -q "$interface_1" "/etc/network/interfaces"; then
+            interface=${interface_1}
+            return
+        elif grep -q "$interface_2" "/etc/network/interfaces"; then
+            interface=${interface_2}
+            return
+        else
+            interfaces_list=$(ip addr show | awk '/^[0-9]+: [^lo]/ {print $2}' | cut -d ':' -f 1)
+            interface=""
+            for iface in $interfaces_list; do
+                if [[ "$iface" = "$interface_1" || "$iface" = "$interface_2" ]]; then
+                    interface="$iface"
+                fi
+            done
+            if [ -z "$interface" ]; then
+                interface="eth0"
+            fi
+            return
+        fi
+    else
+        interface="eth0"
+        return
+    fi
+    _red "Physical interface not found, exit execution"
+    _red "找不到物理接口，退出执行"
+    exit 1
+}
+
+is_private_ipv6() {
+    local address=$1
+    # 输入不含:符号
+    if [[ $ip_address != *":"* ]]; then
+        return 0
+    fi
+    # 输入为空
+    if [[ -z $ip_address ]]; then
+        return 0
+    fi
+    # 检查IPv6地址是否以fe80开头（链接本地地址）
+    if [[ $address == fe80:* ]]; then
+        return 0
+    fi
+    # 检查IPv6地址是否以fc00或fd00开头（唯一本地地址）
+    if [[ $address == fc00:* || $address == fd00:* ]]; then
+        return 0
+    fi
+    # 检查IPv6地址是否以2001:db8开头（文档前缀）
+    if [[ $address == 2001:db8* ]]; then
+        return 0
+    fi
+    # 检查IPv6地址是否以::1开头（环回地址）
+    if [[ $address == ::1 ]]; then
+        return 0
+    fi
+    # 检查IPv6地址是否以::ffff:开头（IPv4映射地址）
+    if [[ $address == ::ffff:* ]]; then
+        return 0
+    fi
+    # 检查IPv6地址是否以2002:开头（6to4隧道地址）
+    if [[ $address == 2002:* ]]; then
+        return 0
+    fi
+    # 检查IPv6地址是否以2001:开头（Teredo隧道地址）
+    if [[ $address == 2001:* ]]; then
+        return 0
+    fi
+    # 其他情况为公网地址
+    return 1
+}
+
+check_ipv6() {
+    IPV6=$(ip -6 addr show | grep global | awk '{print $2}' | cut -d '/' -f1 | head -n 1)
+    if is_private_ipv6 "$IPV6"; then # 由于是内网IPV6地址，需要通过API获取外网地址
+        IPV6=""
+        API_NET=("ipv6.ip.sb" "https://ipget.net" "ipv6.ping0.cc" "https://api.my-ip.io/ip" "https://ipv6.icanhazip.com")
+        for p in "${API_NET[@]}"; do
+            response=$(curl -sLk6m8 "$p" | tr -d '[:space:]')
+            if [ $? -eq 0 ] && ! (echo "$response" | grep -q "error"); then
+                IPV6="$response"
+                break
+            fi
+            sleep 1
+        done
+    fi
+    echo $IPV6 >/usr/local/bin/docker_check_ipv6
+}
+
+if [ ! -d /usr/local/bin ]; then
+    mkdir -p /usr/local/bin
+fi
+statistics_of_run-times
+_green "脚本当天运行次数:${TODAY}，累计运行次数:${TOTAL}"
 check_update
-if [ ! -e '/usr/bin/curl' ]; then
+if ! command -v sudo >/dev/null 2>&1; then
+    _yellow "Installing sudo"
+    ${PACKAGE_INSTALL[int]} sudo
+fi
+if ! command -v curl >/dev/null 2>&1; then
     _yellow "Installing curl"
     ${PACKAGE_INSTALL[int]} curl
+fi
+if ! command -v jq >/dev/null 2>&1; then
+    _yellow "Installing jq"
+    ${PACKAGE_INSTALL[int]} jq
 fi
 if ! command -v dos2unix >/dev/null 2>&1; then
     _yellow "Installing dos2unix"
     ${PACKAGE_INSTALL[int]} dos2unix
 fi
-if [ $? -ne 0 ]; then
-    apt-get -f install >/dev/null 2>&1
-    ${PACKAGE_INSTALL[int]} curl
+if ! command -v lshw >/dev/null 2>&1; then
+    _yellow "Installing lshw"
+    ${PACKAGE_INSTALL[int]} lshw
 fi
 if ! command -v docker >/dev/null 2>&1; then
     _yellow "Installing docker"
@@ -91,23 +207,73 @@ if ! command -v docker-compose >/dev/null 2>&1; then
     chmod +x /usr/local/bin/docker-compose
     docker-compose --version
 fi
-if ! command -v jq >/dev/null 2>&1; then
-    _yellow "Installing jq"
-    ${PACKAGE_INSTALL[int]} jq
-fi
-if ! command -v sudo >/dev/null 2>&1; then
-    _yellow "Installing sudo"
-    ${PACKAGE_INSTALL[int]} sudo
-fi
 ${PACKAGE_INSTALL[int]} openssl
 curl -sL https://raw.githubusercontent.com/spiritLHLS/docker/main/scripts/ssh.sh -o ssh.sh && chmod +x ssh.sh && dos2unix ssh.sh
 curl -sL https://raw.githubusercontent.com/spiritLHLS/docker/main/scripts/alpinessh.sh -o alpinessh.sh && chmod +x alpinessh.sh && dos2unix alpinessh.sh
-statistics_of_run-times
-_green "脚本当天运行次数:${TODAY}，累计运行次数:${TOTAL}"
-# if lsmod | grep -q xfs; then
-#     _green "xfs file system is being used, you can limit disk of docker."
-#     _green "xfs文件系统正在使用，可以限制docker的磁盘大小"
-# else
-#     _yellow "xfs file system is not being used, you can not limit disk of docker."
-#     _yellow "xfs文件系统无法使用，不能限制docker的磁盘大小"
-# fi
+
+# 检测物理接口
+interface_1=$(lshw -C network | awk '/logical name:/{print $3}' | sed -n '1p')
+interface_2=$(lshw -C network | awk '/logical name:/{print $3}' | sed -n '2p')
+check_interface
+
+# 检测IPV6相关的信息
+if [ ! -f /usr/local/bin/docker_check_ipv6 ] || [ ! -s /usr/local/bin/docker_check_ipv6 ] || [ "$(sed -e '/^[[:space:]]*$/d' /usr/local/bin/docker_check_ipv6)" = "" ]; then
+    check_ipv6
+fi
+if [ ! -f /usr/local/bin/docker_ipv6_prefixlen ] || [ ! -s /usr/local/bin/docker_ipv6_prefixlen ] || [ "$(sed -e '/^[[:space:]]*$/d' /usr/local/bin/docker_ipv6_prefixlen)" = "" ]; then
+    ipv6_prefixlen=$(ifconfig ${interface} | grep -oP 'prefixlen \K\d+' | head -n 1)
+    echo "$ipv6_prefixlen" >/usr/local/bin/docker_ipv6_prefixlen
+fi
+if [ ! -f /usr/local/bin/docker_ipv6_gateway ] || [ ! -s /usr/local/bin/docker_ipv6_gateway ] || [ "$(sed -e '/^[[:space:]]*$/d' /usr/local/bin/docker_ipv6_gateway)" = "" ]; then
+    ipv6_gateway=$(ip -6 route show | awk '/default via/{print $3}' | head -n1)
+    echo "$ipv6_gateway" >/usr/local/bin/docker_ipv6_gateway
+fi
+ipv6_address=$(cat /usr/local/bin/docker_check_ipv6)
+ipv6_address_without_last_segment="${ipv6_address%:*}:"
+ipv6_prefixlen=$(cat /usr/local/bin/docker_ipv6_prefixlen)
+ipv6_gateway=$(cat /usr/local/bin/docker_ipv6_gateway)
+
+# 检测docker的配置文件
+if [ ! -z "$ipv6_address" ] && [ ! -z "$ipv6_prefixlen" ] && [ ! -z "$ipv6_gateway" ] && [ ! -z "$ipv6_address_without_last_segment" ]; then
+    if [ ! -f /etc/docker/daemon.json ]; then
+        touch /etc/docker/daemon.json
+    fi
+    json_content="{
+        \"ipv6\": true,
+        \"fixed-cidr-v6\": \"$ipv6_address_without_last_segment/$ipv6_prefixlen\"
+    }"
+    echo "$json_content" > /etc/docker/daemon.json
+    # 设置允许IPV6转发
+    sysctl_path=$(which sysctl)
+    $sysctl_path -w net.ipv6.conf.all.forwarding=1
+    $sysctl_path -w net.ipv6.conf.all.proxy_ndp=1
+    $sysctl_path -w net.ipv6.conf.default.proxy_ndp=1
+    $sysctl_path -w net.ipv6.conf.docker0.proxy_ndp=1
+    $sysctl_path -w net.ipv6.conf.${interface}.proxy_ndp=1
+    $sysctl_path -f
+    # https://github.com/DanielAdolfsson/ndppd/blob/master/ndppd.conf-dist 参考配置
+    ${PACKAGE_INSTALL[int]} ndppd
+cat <<EOT > /etc/ndppd.conf
+route-ttl 30000
+address-ttl 30000
+proxy ${interface} {
+   router yes
+   timeout 500
+   autowire no
+   keepalive yes
+   retries 3
+   promiscuous no
+   ttl 30000
+   rule ${ipv6_address_without_last_segment}/${ipv6_prefixlen} {
+      static
+      autovia no
+   }
+}
+EOT
+systemctl restart docker
+sleep 3
+systemctl restart ndppd
+fi
+sleep 3
+systemctl status docker 2>/dev/null
+systemctl status ndppd 2>/dev/null
