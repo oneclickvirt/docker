2ae4c821c2c084e598ba7723d09149e2f9a93f9c
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-08-24 15:49:46 +0800

    Update dockerinstall.sh

--- scripts/dockerinstall.sh
+++ scripts/dockerinstall.sh
@@ -282,16 +282,6 @@ ipv6_gateway=$(cat /usr/local/bin/docker_ipv6_gateway)
 
 # 检测docker的配置文件
 if [ ! -z "$ipv6_address" ] && [ ! -z "$ipv6_prefixlen" ] && [ ! -z "$ipv6_gateway" ] && [ ! -z "$ipv6_address_without_last_segment" ]; then
-    if [ ! -f /etc/docker/daemon.json ]; then
-        touch /etc/docker/daemon.json
-    fi
-    json_content="{
-        \"ipv6\": true,
-        \"fixed-cidr-v6\": \"$ipv6_address_without_last_segment/$ipv6_prefixlen\"
-    }"
-        # \"ip6tables\": true,
-        # \"experimental\": true
-    echo "$json_content" >/etc/docker/daemon.json
     # 设置允许IPV6转发
     sysctl_path=$(which sysctl)
     $sysctl_path -w net.ipv6.conf.all.forwarding=1
@@ -321,6 +311,7 @@ if [ ! -z "$ipv6_address" ] && [ ! -z "$ipv6_prefixlen" ] && [ ! -z "$ipv6_gatew
         line_number=6
         sed -i "${line_number}s|.*|${new_exec_start}|" "$file_path"
     fi
+    docker network create --ipv6 --subnet=172.26.0.0/16 --subnet=${ipv6_address_without_last_segment}/${ipv6_prefixlen} ipv6_subnet
 fi
 if [ -f "/usr/local/bin/ndpresponder" ]; then
     systemctl daemon-reload
