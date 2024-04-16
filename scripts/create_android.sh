ee865248e455a329bc8bd655678ff65e35d5e5cd
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-04-16 23:25:24 +0800

    Update create_android.sh

--- scripts/create_android.sh
+++ scripts/create_android.sh
@@ -83,7 +83,9 @@ if [ -z "$user_password" ]; then
 fi
 if ! command -v npm >/dev/null 2>&1; then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-    source ~/.bashrc
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
     nvm install 20
     echo "node version:"
     node -v
