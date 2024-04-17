cbffbb5403577d8bc316c30c64b545539db498d9
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-04-17 12:34:28 +0800

    Update nohup-ws-scrcpy.sh

--- extra_scripts/nohup-ws-scrcpy.sh
+++ extra_scripts/nohup-ws-scrcpy.sh
@@ -7,6 +7,6 @@ export NODE_OPTIONS=--openssl-legacy-provider
 export PATH=
 echo $PATH
 killall adb
-rm -rf nohup.out
-nohup adb connect localhost:5555 & 
+rm -rf adb-nohup.out
+nohup adb connect localhost:5555 > adb-nohup.out & 
 npm start
